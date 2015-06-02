ea6830565b7af529737410eff55aa32c03a69470
Patryk Nowak <patrik.nowak@gmail.com>
2015-06-02 23:13:57 +0100

    Validation: checking if email already exist in database

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var userService = require('../services/user-service');
 
 var userSchema = new Schema({
 	firstName: {type: String, required: 'Please enter your first name'},
@@ -8,6 +9,16 @@ var userSchema = new Schema({
 	password: {type: String, required: 'Please enter your password'},
 	created: {type:Date, default: Date.now}
 });
+
+userSchema.path('email').validate(function(value, next){
+	userService.findUser(value, function(err, user){
+		if(err) {
+			console.log(err);
+			return next(false);
+		}
+		next(!user);
+	})
+}, 'That email is already in use');
 
 var User = mongoose.model('User', userSchema);
 
